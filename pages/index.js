67ac33c199b7d381667cc0343ab56f3ad35c3edd
devRabbani <dev.golamrabbani@gmail.com>
2022-07-31 21:38:38 +0530

    adding bg circle

--- pages/index.js
+++ pages/index.js
@@ -10,16 +10,19 @@ export default function Home() {
         {plantsData.map((item, i) => (
           <Link href={`/predict/${item}`} key={i}>
             <a className={styles.card}>
-              <div className={styles.photo}>
-                <Image
-                  className={styles.img}
-                  src={`/assets/${item}.svg`}
-                  alt={item}
-                  width='100%'
-                  height='100%'
-                  layout='responsive'
-                />
+              <div className={styles.photoWrapper}>
+                <div className={styles.photo}>
+                  <Image
+                    className={styles.img}
+                    src={`/assets/${item}.svg`}
+                    alt={item}
+                    width='100%'
+                    height='100%'
+                    layout='responsive'
+                  />
+                </div>
               </div>
+
               <p className={styles.name}>{item}</p>
             </a>
           </Link>
